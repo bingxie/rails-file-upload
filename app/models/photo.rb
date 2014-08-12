b0f89d075d474ab9c525ba1ba699b02b8c2e73c3
Bing Xie <bing.xie78@gmail.com>
2014-08-12 16:19:43 +1000

    Use jquery file upload to upload mutiple files

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,10 @@
 class Photo < ActiveRecord::Base
   belongs_to :album
   mount_uploader :image, ImageUploader
+
+  before_create :default_name
+
+  def default_name
+    self.name ||= File.basename(image.filename, '.*').titleize if image
+  end
 end
